f82c6495769eb76c04c4ad3ad43e893fb4acf17a
vucaoson <sonnhthtb1610@gmail.com>
2019-12-01 10:37:11 +0700

    add test.cpp to Github

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define whatIs(a) cerr << #a " is " << (a) << endl;
+using namespace std;
+const int N = 200005;
+const long long base = 1e9+7;
+const long long inf = 1e18+7;
+void Solve(){
+
+}
+int main(){
+    int T=1;
+    cin >> T;
+    while(T--){
+        Solve();
+    }
+    return 0;
+}
